e631865cf954fe85c4775bfe961e3a0af5ef8573
pomemaruko <pomemaruko@gmail.com>
2016-10-26 14:43:14 +0900

    YOUUTBE・ニコ動の簡易クローラー

---
+++ nico.rb
@@ -0,0 +1,38 @@
+﻿require 'nokogiri'
+require 'anemone'
+require 'certified'
+
+opts = {
+  :depth_limit => 5,
+}
+
+urls = []
+# キーワードを入力
+search_term = URI.encode("")
+url = "http://www.nicovideo.jp/search/#{search_term}?f_range=0&l_range=0&opt_md="
+
+
+Anemone.crawl(url, opts) do |anemone|
+# 次にクロールするlinkを指定
+anemone.focus_crawl do |page|
+ page.links.keep_if { |link|
+   link.to_s.match(/#{search_term}\?page=[1-9]&sort=h&order=d/) # ここ正規表現。いまだと起点となるページから飛べるページの内、urlに/01/が含まれるページのみしかpageを回収しないよう予め指定しています。
+ } 
+end
+anemone.on_every_page do |page|
+
+doc = Nokogiri::HTML(open(url), nil,"UTF-8")
+doc.xpath("//li[contains(@class, 'item')]").each do |node|
+title = node.xpath("./div[contains(@class, 'itemContent')]/p[contains(@class, 'itemTitle')]/a").text
+puts title
+html = node.xpath("//div[contains(@class, 'itemThumb')]/a").attribute("href").value
+puts "<a href='http://www.nicovideo.jp/#{html}'>#{title}</a>"
+puts "<br>"
+img = node.xpath("//img[contains(@class, 'thumb')]").attribute("src").value
+puts img
+puts "<br>"
+end
+puts page.url
+puts "<br>"
+end
+end
---
+++ youtube.rb
@@ -0,0 +1,39 @@
+﻿require 'nokogiri'
+require 'anemone'
+require 'certified'
+
+opts = {
+  :depth_limit => 5,
+}
+
+urls = []
+# キーワードを入力
+search_term = URI.encode("")
+url = "https://www.youtube.com/results?sp=CANIAOoDAA%253D%253D&q=#{search_term}"
+
+
+Anemone.crawl(url, opts) do |anemone|
+# 次にクロールするlinkを指定
+anemone.focus_crawl do |page|
+ page.links.keep_if { |link|
+   link.to_s.match(/CANI[A-Z]ouOoDAA%253D%253D&q=#{search_term}/) # ここ正規表現。いまだと起点となるページから飛べるページの内、urlに/01/が含まれるページのみしかpageを回収しないよう予め指定しています。
+ } 
+end
+anemone.on_every_page do |page|
+
+doc = Nokogiri::HTML(open(url), nil,"UTF-8")
+doc.xpath("//div[contains(@class, 'yt-lockup-dismissable yt-uix-tile')]").each do |node|
+title = node.xpath("./div[contains(@class, 'yt-lockup-content')]/h3[contains(@class, 'yt-lockup-title ')]/a").text
+html = node.css("a").attribute("href").value 
+puts "<h4><a href='https://www.youtube.com#{html}' target='_blank' rel='nofollow'>#{title} 【提供元】 - YouTube</a>"
+html = html.gsub(/\/watch\?v=/, '') 
+puts "<a href='https://www.youtube.com/watch?v=#{html}' target='_blank' rel='nofollow'><img src='http://img.youtube.com/vi/#{html}/hqdefault.jpg'></a></h4>"
+puts "<br>"
+end
+tuduki = page.url
+puts  "<h4><a href='#{tuduki}' target='_blank' rel='nofollow'>"
+puts "もっと見たい方はコチラへ"
+puts "</a></h4>"
+puts "<br>"
+end
+end
