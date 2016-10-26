819a593b51ee087ac584d9ec5e79f546d9800500
pomemaruko <pomemaruko@gmail.com>
2016-10-26 15:00:12 +0900

    cakephp3 - チュートリアル（ミニブログ勉強用）

---
+++ samplecake/tests/TestCase/Controller/ArticlesControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Test\TestCase\Controller;
+
+use App\Controller\ArticlesController;
+use Cake\TestSuite\IntegrationTestCase;
+
+/**
+ * App\Controller\ArticlesController Test Case
+ */
+class ArticlesControllerTest extends IntegrationTestCase
+{
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'app.articles'
+    ];
+
+    /**
+     * Test index method
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
